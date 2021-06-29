a668af44034b9eda21753f147f683c5a32622b06
bryt0 <85228422+bryt0@users.noreply.github.com>
2021-06-29 09:38:08 +0100

    Update jsonplaceolder.js

--- task/jsonplaceolder.js
+++ task/jsonplaceolder.js
@@ -14,3 +14,32 @@ async function getUsersName () {
 
 
         getUsersName()
+
+
+// const https = require('https');
+
+// let request = https.get('https://jsonplaceholder.typicode.com/users', (res) => {
+//   if (res.statusCode !==200) {
+//     console.error(`INVALID REQUEST: ${res.statusCode}`);
+//     res.resume();
+//     return;
+//   }
+//   let data = '';
+
+//   res.on('data', (chunk) => {
+//     data += chunk;
+//   })
+
+
+//   res.on('end', () => {
+//     data = JSON.parse(data);
+//     console.log('data retrieved ');
+//     console.log(JSON.parse(data));
+    
+//     for (let i = 0; i < data.length; i++) {
+//       console.log(data[i].name);
+//     }
+
+//   })
+
+// });
