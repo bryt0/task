9eb8fb0beac22b576fc747c49fdcebd541305688
bryt0 <85228422+bryt0@users.noreply.github.com>
2021-07-12 19:55:54 +0100

    E-Commerce App Updated
    correct http_request using appropraite verbs

--- weeksix/index.js
+++ weeksix/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const product = require('./models/product');
+// const {v4:uuidv4} = require ('uuid'); 
 
 const PORT = 8100; //change port if it gives EADDRINUSE error
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));//to return string
 
 // read all records
 app.get('/product', (req, res) =>{
@@ -11,37 +13,41 @@ app.get('/product', (req, res) =>{
 });
 
 // create new record
-app.post('/product', (req, res) => {
-    const { name, desc , image , price } = req.body
-    if (!name || !desc || !image || !price) {
-      return  res
-        .status(400)
-        .json({ success: false, msg: 'please input product details' })
-    }
-
-    const newProduct = { id : product.length + 1  , name: name, desc: desc, image: image, price: price}
-    product.push(newProduct);
-    res
-    .status(200)
-    .json({ success: true, data: product})  
+app.post('/product/create', (req, res) => {
+    console.log (req.body)
+let newProduct = {
+    // id: uuidv4(),
+    id: product.length + 1,
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price
+}
+list.push(newProduct);
+res.json(product)
+ 
 })
 
-// update record
-app.put('/product/:id', (req, res) =>{
-const productID = Number(req.params.id);
-const product = product.find((product) => product.id === productID);
-const index = product.indexOf(product);
-
-if (!product) {
-    res
-    .status(500)
-    .send("product not found");
-}else {
-    const updatedProduct = {...product, ...req.body}
+app.put("/product/:id", (req, res) => {
+    const { id } = req.params;
+    // find product by id, return 404 if not found
+    product = product.find((product) => product.id === Number(id));
+    if (!product) {
+      return res
+        .status(404)
+        .json({ success: false, 
+            msg: `Product with id ${id} does not exist.` });
+    }
+    
+    const index = product.indexOf(product);
+    const updatedProduct = { ...product, ...req.body };
+  
     product[index] = updatedProduct;
-    res.json(product)
-};
-});
+    return res.status(200).json({
+      success: true,
+      msg: `Product ${id} has been sucessfully added.`,
+      data: product,
+    });
+  });
 
 // delete record
 app.delete('/product/:id',(req,res) =>{
